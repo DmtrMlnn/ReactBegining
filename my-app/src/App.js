4798410e21b958bab9fb147c17c7c9eef29ff9ec
Dmtr_Mlnn <dmtrmlnn@gmail.com>
2024-07-28 13:32:47 +0300

    after jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,42 @@
-import logo from './logo.svg'; //Декларативный стиль
-import './App.css'; //Декларативный стиль
-import { createElement } from 'react';
+import logo from "./logo.svg"; //Декларативный стиль
+import "./App.css"; //Декларативный стиль
+import { createElement } from "react";
 
-export const App = () => { //Императивный стиль
+export const App = () => {
+	//Императивный стиль
 	const year = new Date().getFullYear();
-  return (//Декларативный стиль
+	return (
+		//Декларативный стиль
 		createElement(
 			`div`,
-			{className:`App-header`},
+			{ className: `App-header` },
 			createElement(
 				`header`,
-				{className:"App-header"},
-				createElement(`img`,{src:`${logo}`,className:"App-logo",alt:`logo`}),
-				createElement(`p`,null,`Edit `,createElement(`code`, null, `src/App.js`),` and save to reload.`),
-				createElement(`a`,{className:"App-link", href:"https://reactjs.org", target:"_blank", rel:"noopener noreferrer"},`Learn React`),
+				{ className: "App-header" },
+				createElement(`img`, {
+					src: `${logo}`,
+					className: "App-logo",
+					alt: `logo`,
+				}),
+				createElement(
+					`p`,
+					null,
+					`Edit `,
+					createElement(`code`, null, `src/App.js`),
+					` and save to reload.`
+				),
+				createElement(
+					`a`,
+					{
+						className: "App-link",
+						href: "https://reactjs.org",
+						target: "_blank",
+						rel: "noopener noreferrer",
+					},
+					`Learn React`
+				),
 				createElement(`div`, null, year)
 			)
 		)
-  );
-}
+	);
+};
