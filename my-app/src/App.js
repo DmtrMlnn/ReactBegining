ea96bb0295b85e6abc41de5205141f9a90814958
Dmtr_Mlnn <dmtrmlnn@gmail.com>
2024-07-28 13:29:37 +0300

    eforejsx`

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg'; //Декларативный стиль
+import './App.css'; //Декларативный стиль
+import { createElement } from 'react';
+
+export const App = () => { //Императивный стиль
+	const year = new Date().getFullYear();
+  return (//Декларативный стиль
+		createElement(
+			`div`,
+			{className:`App-header`},
+			createElement(
+				`header`,
+				{className:"App-header"},
+				createElement(`img`,{src:`${logo}`,className:"App-logo",alt:`logo`}),
+				createElement(`p`,null,`Edit `,createElement(`code`, null, `src/App.js`),` and save to reload.`),
+				createElement(`a`,{className:"App-link", href:"https://reactjs.org", target:"_blank", rel:"noopener noreferrer"},`Learn React`),
+				createElement(`div`, null, year)
+			)
+		)
+  );
+}
